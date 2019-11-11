04bf33bcff85eb0ecd80c69ff809f6e1bd02479b
Couch NUC <williamchen755@gmail.com>
2019-11-11 14:14:41 +1100

    Added install script for gazebo plugins

---
+++ scripts/install_gazebo_plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -f install_lock || $1 == -f ]]; then
+    # Check if in right directory. This should be run in root of workspace.
+    if [ -d "src" ]; then
+        cd src
+        # Perform installation instructions
+        sudo apt-get install ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control
+    else
+        echo "This file needs to be run in root of workspace."
+    fi
+else
+    # Install lock doesn't exist; don't install here
+    echo "This file must be called by the install script. Use -f to override."
+fi
